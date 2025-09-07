377b7bd72553f744f1fc433fd6ebb5766bf048a6
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2025-09-07 18:22:31 +0530

    Spring Boot: LightWeight Java LLM: initial setup

---
+++ src/main/java/com/example/javaLLM/JavaLlmApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaLLM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLlmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLlmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaLLM/JavaLlmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaLLM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLlmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
