1d226f5f7656ca249dc6daeba28d49d4ee2d7263
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2025-09-07 18:30:43 +0530

    Spring Boot: LightWeight Java LLM: added TextRequest.java model implementation

---
+++ src/main/java/com/example/javaLLM/service/TextRequest.java
@@ -0,0 +1,13 @@
+package com.example.javaLLM.service;
+
+public class TextRequest {
+    private String input;
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+}
